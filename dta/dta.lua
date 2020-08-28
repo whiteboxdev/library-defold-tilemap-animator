10cc79c5ce3e05edbc8b84ec16382bbf266afb70
Klayton Kowalski <gymratgames@gmail.com>
2020-08-27 20:54:36 -0500

    Fixed example project for asset redesign.

--- dta/dta.lua
+++ dta/dta.lua
@@ -1,3 +1,7 @@
+----------------------------------------------------------------------
+-- LICENSE
+----------------------------------------------------------------------
+
 -- MIT License
 
 -- Copyright (c) 2020 Klayton Kowalski
@@ -19,6 +23,8 @@
 -- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 -- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 -- SOFTWARE.
+
+-- https://github.com/gymratgames/defold-tilemap-animator
 
 ----------------------------------------------------------------------
 -- DEPENDENCIES
--- example/animation_groups.lua
+++ example/animation_groups.lua
@@ -1,19 +1,3 @@
--- RETURN USER-CREATED ANIMATION GROUPS.
-
--- Format:    key       = <start_tile> (tilesource number)
---            { value } = <end_tile>   (tilesource number)
---                        <playback>   (animation style)
---                        <step>       (frame duration in seconds)
-
--- Playbacks: "loop_forward"
---            "loop_backward"
---            "loop_pingpong"
---            "loop_corolla"
---            "once_forward"
---            "once_backward"
---            "once_pingpong"
---            "once_corolla"
-
 return {
 	[1]  = { end_tile = 9,  playback = "loop_forward",  step = 0.55  },
 	[11] = { end_tile = 19, playback = "loop_backward", step = 0.55  },
