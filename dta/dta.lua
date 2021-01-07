3017742b9091cea6998ed825f0e61a51f418811d
Klayton Kowalski <klaytonkowalski@gmail.com>
2021-01-07 17:39:55 -0600

    Txt update.

--- dta/dta.lua
+++ dta/dta.lua
@@ -24,7 +24,7 @@
 -- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 -- SOFTWARE.
 
--- https://github.com/kowalskigamedevelopment/defold-tilemap-animator
+-- https://github.com/klaytonkowalski/defold-tilemap-animator
 
 ----------------------------------------------------------------------
 -- PROPERTIES
